747d1e3e1a599052e2aaefc5c652c0f26057f5eb
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-11-22 16:36:35 +0900

    cy 설정 수정

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,15 +1,5 @@
 describe("template spec", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
-
-  it('should contain "Get started by ~" text', () => {
-    cy.contains("Get started by editing");
-  });
-
-  it('should contain "Learn" heading anchor with correct href prop', () => {
-    cy.contains("Learn")
-      .should("have.attr", "href")
-      .and("include", "https://nextjs.org/learn");
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
   });
 });
