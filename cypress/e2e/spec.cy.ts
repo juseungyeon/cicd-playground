59bbea7ddedb1f767b745ca42fa40771b75a54cb
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-11-22 16:47:40 +0900

    feat: cy e2e테스트 코드 수정

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,15 @@
 describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it('should contain "Get started by ~" text', () => {
+    cy.contains("Get started by editing");
+  });
+
+  it('should contain "Learn" heading anchor with correct href prop', () => {
+    cy.contains("Learn")
+      .should("have.attr", "href")
+      .and("include", "https://nextjs.org/learn");
   });
 });
